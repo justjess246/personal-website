7cb562490ec4c1492c25d564efdb07da3cc66bf3
justjess246 <jbjb2200@gmail.com>
2021-09-06 11:44:44 -0700

    Adding mobile view to navigation bar

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -1,43 +1,68 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { 
     NavBar, 
     NavBarContainer, 
     NavLogo, 
     NavIcon,
+    MobileIcon,
     NavMenu,
     NavItem,
-    NavLink
+    NavLink,
+    NavBtnLink
  } from './NavigationBarElements';
+ import { IconContext } from 'react-icons/lib';
+ import { FaBars, FaTimes } from 'react-icons/fa';
 
 const NavigationBar = () => {
-    const [click, setCLick] = useState(false)
+    const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
 
-    const handleClick = () => setCLick(!click)
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if (window.innerWidth <= 960) {
+          setButton(false);
+        } else {
+          setButton(true);
+        }
+    };
+
+    useEffect(() => {
+        showButton();
+    }, []);
+    
+    window.addEventListener('resize', showButton);
 
     return (
         <div>
-            <NavBar>
-                <NavBarContainer>
-                    <NavLogo to="/">
-                        <NavIcon />
-                        JB
-                    </NavLogo>
-                    <NavMenu onCLick={handleClick}>
-                        <NavItem>
-                            <NavLink to='/'>Home</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink smooth to={'/page#projects'}>Projects</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink smooth to={'/page#art'}>Art</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink smooth to={'/page#contact'}>Contact</NavLink>
-                        </NavItem>
-                    </NavMenu>
-                </NavBarContainer>
-            </NavBar>
+            <IconContext.Provider value={{ color: '#fff' }}>
+                <NavBar>
+                    <NavBarContainer>
+                        <NavLogo to="/" onClick={closeMobileMenu}>
+                            <NavIcon />
+                            JB
+                        </NavLogo>
+                        <MobileIcon onClick={handleClick}>
+                            {click ? <FaTimes /> : <FaBars />}
+                        </MobileIcon>
+                        <NavMenu onCLick={handleClick} click={click}>
+                            <NavItem>
+                                <NavLink to='/'>Home</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink smooth to={'/page#projects'} onClick={closeMobileMenu}>Projects</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink smooth to={'/page#art'} onClick={closeMobileMenu}>Art</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink smooth to={'/page#contact'} onClick={closeMobileMenu}>Contact</NavLink>
+                            </NavItem>
+                        </NavMenu>
+                    </NavBarContainer>
+                </NavBar>
+            </IconContext.Provider>
         </div>
     )
 }
